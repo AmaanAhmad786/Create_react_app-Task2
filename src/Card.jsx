eb025ae4a4ac960a1c0b3e3e4af079f087237532
Amaan Ahmad <amaanahmad330@gmail.com>
2023-08-22 18:07:55 +0530

    Change in Card.jsx file

--- src/Card.jsx
+++ src/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
       <div className="card_container">
         <h4>Id: {props.id}</h4>
         <img src={props.avatar} alt="img" />
-        <h3>Name: {props.first_name + " " + props.last_name}</h3>
+        <h3>UserName: {props.first_name + " " + props.last_name}</h3>
         <h3>Email: {props.email}</h3>
       </div>
     </>
